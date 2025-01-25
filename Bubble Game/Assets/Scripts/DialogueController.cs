a918bf7f96ccdbc068b5afe3535646fb6504e4f6
azahin1 <z4hin.4brar@gmail.com>
2025-01-24 23:35:18 -0700

    add display method, will need to change later

--- Bubble Game/Assets/Scripts/DialogueController.cs
+++ Bubble Game/Assets/Scripts/DialogueController.cs
@@ -1,5 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class DialogueController : MonoBehaviour {
+    private string dialogueContent = "";
+    private TextMeshProUGUI textComponent = null;
 
+    private void Start() {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        for (int i = 0; i < 20; i++) {
+            DisplayText("owo? testies? " + "<color=red>" + i.ToString() + "</color>");
+        }
+    }
+
+    private void DisplayText(string text) {
+        dialogueContent += "\n" + text;
+        textComponent.text = dialogueContent;
+    }
 }
