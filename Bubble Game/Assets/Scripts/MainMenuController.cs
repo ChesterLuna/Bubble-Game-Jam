4c90815036d14d8d6f35c74ead298e4270d7b1c5
azahin <z4hin.4brar@gmail.com>
2025-01-25 15:49:08 -0700

    add functionality to the options button

--- Bubble Game/Assets/Scripts/MainMenuController.cs
+++ Bubble Game/Assets/Scripts/MainMenuController.cs
@@ -3,13 +3,31 @@ using UnityEngine;
 public class MainMenuController : MonoBehaviour {
     private GameObject optionsMenu = null;
     private GameObject credits = null;
+    private GameObject startButton = null;
+    private GameObject optionsButton = null;
+    private GameObject creditsButton = null;
 
     private void Start() {
         optionsMenu = transform.Find("OptionsMenu").gameObject;
         credits = transform.Find("CreditsCanvas").gameObject;
+        startButton = transform.Find("StartGameButton").gameObject;
+        optionsButton = transform.Find("OptionsButton").gameObject;
+        creditsButton = transform.Find("CreditsButton").gameObject;
         optionsMenu.SetActive(false);
         credits.SetActive(false);
-        Debug.Log(optionsMenu);
-        Debug.Log(credits);
+    }
+
+    public void OpenOptions() {
+        optionsMenu.SetActive(true);
+        startButton.SetActive(false);
+        optionsButton.SetActive(false);
+        creditsButton.SetActive(false);
+    }
+
+    public void CloseOptions() {
+        optionsMenu.SetActive(false);
+        startButton.SetActive(true);
+        optionsButton.SetActive(true);
+        creditsButton.SetActive(true);
     }
 }
