018dafff9b94f5b800bb41b92c803cde082eaf34
ChesterLuna <chester99124@gmail.com>
2025-01-25 00:58:29 -0700

    feat: updated creator with dummy functions

--- Bubble Game/Assets/Scripts/Clickable/ClickableCreator.cs
+++ Bubble Game/Assets/Scripts/Clickable/ClickableCreator.cs
@@ -5,9 +5,27 @@ public class ClickableCreator : MonoBehaviour, ClickableAsset
     // Intented to be used for buttons that create objects
 
     [SerializeField] GameObject toInstantiate;
+    GameObject objectInstantiated;
 
     public void OnClick()
     {
         print("Clicked on clickable creator, this should instantiate a game object as a child");
     }
+
+
+    // While this is "creating the object"
+    // I think it would be useful to just show and hide an specific object
+
+    private void CreateAsset()
+    {
+        print("Create the toInstantiate game object");
+        // Assign the created object to objectInstantiated
+    }
+
+    private void DestroyAsset()
+    {
+        print("Dekete the toInstantiate game object");
+        // Destroy the created object in objectInstantiated
+    }
+
 }
