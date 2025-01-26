8d9f463ed4ec4ed4c43b43dd4ea49fad26274610
ChesterLuna <chester99124@gmail.com>
2025-01-26 00:21:45 -0700

    feat: added timer counter

---
+++ Bubble Game/Assets/TimerController.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    private TextMeshProUGUI textComponent = null;
+    [SerializeField] GameObject levelManager;
+
+    private void Start()
+    {
+        textComponent = GetComponent<TextMeshProUGUI>();
+        levelManager = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    private void Update()
+    {
+        textComponent.text = ConvertToMinutes(levelManager.GetComponent<Timer>().timeLeft);
+    }
+
+    private string ConvertToMinutes(float seconds)
+    {
+        int minutesLeft = Mathf.FloorToInt(seconds/60f);
+        int secondsLeft = Mathf.FloorToInt(seconds - minutesLeft * 60f);
+        string timerFormat = string.Format("{0:0}:{1:00}", minutesLeft, secondsLeft);
+        return timerFormat;
+    }
+}
