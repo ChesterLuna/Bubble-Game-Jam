37a65f7e5ba6c97c61b4fc293aef1d0114e9dfc7
ChesterLuna <chester99124@gmail.com>
2025-01-26 11:30:01 -0700

    fix: timer doesnt go negative

--- Bubble Game/Assets/TimerController.cs
+++ Bubble Game/Assets/TimerController.cs
@@ -19,6 +19,7 @@ public class TimerController : MonoBehaviour
 
     private string ConvertToMinutes(float seconds)
     {
+        if(seconds < 0) return "0:00"
         int minutesLeft = Mathf.FloorToInt(seconds/60f);
         int secondsLeft = Mathf.FloorToInt(seconds - minutesLeft * 60f);
         string timerFormat = string.Format("{0:0}:{1:00}", minutesLeft, secondsLeft);
