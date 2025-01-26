f02f703d229dad645d612b4bd6aff15fd88c3014
azahin1 <z4hin.4brar@gmail.com>
2025-01-25 22:32:54 -0700

    restructure canvas UI to have different sections

--- Bubble Game/Assets/Scripts/GuidebookController.cs
+++ Bubble Game/Assets/Scripts/GuidebookController.cs
@@ -2,32 +2,10 @@ using UnityEngine;
 using TMPro;
 
 public class GuidebookController : MonoBehaviour {
-    private string page1;
-    private string page2;
 
     private TextMeshProUGUI textComponent = null;
-    private GameObject nextButton = null;
-    private GameObject prevButton = null;
 
     private void Start() {
         textComponent = transform.Find("GuideText").GetComponent<TextMeshProUGUI>();
-        nextButton = transform.Find("NavigationButtonNext").gameObject;
-        prevButton = transform.Find("NavigationButtonPrev").gameObject;
-        prevButton.SetActive(false);
-        page1 = "Species";
-        page2 = "Toppings";
-        textComponent.text = page1;
-    }
-
-    public void swapPage() {
-        if (nextButton.activeInHierarchy) {
-            nextButton.SetActive(false);
-            prevButton.SetActive(true);
-            textComponent.text = page2;
-        } else {
-            nextButton.SetActive(true);
-            prevButton.SetActive(false);
-            textComponent.text = page1;
-        }
     }
 }
