f8926e62a992b3ed20e52e59e563856ef8801448
azahin1 <z4hin.4brar@gmail.com>
2025-01-27 14:18:43 -0700

    add day 5, 6, and 7

--- Bubble Game/Assets/Scripts/gameplaySequenceHub.cs
+++ Bubble Game/Assets/Scripts/gameplaySequenceHub.cs
@@ -20,7 +20,7 @@ public class GameplaySequenceHub : MonoBehaviour {
             "And if they don't like my drinks, they don't pay!",
             "If it keeps up, I'll be out of business in no time!",
             "I need to set a goal.",
-            "Let's be reasonable: if I can earn 100 gold by the end of the week, I'm sure I can convince the townspeople that The Boba Tavern should be here to stay!"}
+            "Let's be reasonable: if I can earn <color=#E9B300>100 gold</color> by the end of the week, I'm sure I can convince the townspeople that The Boba Tavern should be here to stay!"}
         },
         {3, new string[] {
             "The third day and you know what they say, third time's the charm! I'm sure by the end of the day, I'll convince the creatures of this town that The Boba Tavern is here to stay!"}
@@ -29,11 +29,23 @@ public class GameplaySequenceHub : MonoBehaviour {
             "It's already day 4... I'm halfway through the week.",
             "I'm sure by now news has spread to the Fairies so I can add the Fruit Tea base for them.", 
             "And I've just received new stock!",
-            "For customers who want something <color=#0088AA>Intense</color>, I can add <color=#0088AA>Dragon Tears</color> and to make the Bubble Tea <color=#0088AA>Fluffy</color>, I should add <color=#0088AA>Pixie Clouds</color>. Okay, let's do this!"
-        }},
-        {5, new string[] {}},
-        {6, new string[] {}},
-        {7, new string[] {}}
+            "For customers who want something <color=#0088AA>Intense</color>, I can add <color=#0088AA>Dragon Tears</color> and to make the Bubble Tea <color=#0088AA>Fluffy</color>, I should add <color=#0088AA>Pixie Clouds</color>.",
+            "Okay, let's do this!"}
+        },
+        {5, new string[] {
+            "Alrighty, it's the fifth day! I feel pumped with the dwarf tavern owner's talk and he's right, I gotta keep going! Let's make today a good one!"}
+        },
+        {6, new string[] {
+            "The sixth day... just one more day after this. I gotta keep up the momentum and make sure I finish strong!",
+            "Demons might start coming by so I should serve the Black Tea base for them.", 
+            "The last stock for my toppings have also arrive now.",
+            "For customers who want something <color=#0088AA>Novel</color>, I need to add the <color=#0088AA>Sunbeam Jelly</color> topping and to create a <color=#0088AA>Calming</color> effect, I would need some <color=#0088AA>Magic Beans</color>.",
+            "Got it, I can do it!"}
+        },
+        {7, new string[] {
+            "The last day! I've worked so hard this week and have served so many customers... now it's time to use everything I've learned for this last push!",
+            "I need to reach <color=#E9B300>100 gold</color>"}
+        }
     };
 
     private void Awake() { // making things singleton
