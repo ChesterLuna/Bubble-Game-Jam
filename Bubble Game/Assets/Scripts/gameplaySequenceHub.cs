98e3e254df2034bf6a827f47e0bb6bb954631d92
azahin1 <z4hin.4brar@gmail.com>
2025-01-27 14:11:17 -0700

    add days 2, 3, and 4

--- Bubble Game/Assets/Scripts/gameplaySequenceHub.cs
+++ Bubble Game/Assets/Scripts/gameplaySequenceHub.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class GameplaySequenceHub : MonoBehaviour {
     public static GameplaySequenceHub instance;
+    public string playerName = "<color=#8800AA>You</color>";
 
     public Dictionary<int, string[]> dayScene = new Dictionary<int, string[]> {
         {1, new string[] {
@@ -14,9 +15,22 @@ public class GameplaySequenceHub : MonoBehaviour {
             "Good thing I have my handy <color=#0088AA>Guidebook</color> if I get stuck making an order. Maybe I should look at it now...",
             "Oh look!, my first customer!"}
         },
-        {2, new string[] {}},
-        {3, new string[] {}},
-        {4, new string[] {}},
+        {2, new string[] {
+            "I'm not getting as many customers as I was hoping...",
+            "And if they don't like my drinks, they don't pay!",
+            "If it keeps up, I'll be out of business in no time!",
+            "I need to set a goal.",
+            "Let's be reasonable: if I can earn 100 gold by the end of the week, I'm sure I can convince the townspeople that The Boba Tavern should be here to stay!"}
+        },
+        {3, new string[] {
+            "The third day and you know what they say, third time's the charm! I'm sure by the end of the day, I'll convince the creatures of this town that The Boba Tavern is here to stay!"}
+        },
+        {4, new string[] {
+            "It's already day 4... I'm halfway through the week.",
+            "I'm sure by now news has spread to the Fairies so I can add the Fruit Tea base for them.", 
+            "And I've just received new stock!",
+            "For customers who want something <color=#0088AA>Intense</color>, I can add <color=#0088AA>Dragon Tears</color> and to make the Bubble Tea <color=#0088AA>Fluffy</color>, I should add <color=#0088AA>Pixie Clouds</color>. Okay, let's do this!"
+        }},
         {5, new string[] {}},
         {6, new string[] {}},
         {7, new string[] {}}
